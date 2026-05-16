fc4e18c00b1cb735505a4ad03c4a05e78f3844fa
Kate Houston <khousto4@login2.talapas.uoregon.edu>
2026-05-16 15:36:35 -0700

    Add data prep and dataset scripts

---
+++ data/data_prep.py
@@ -0,0 +1,67 @@
+import os
+import argparse
+import pandas as pd
+from datasets import load_dataset
+from PIL import Image
+
+parser = argparse.ArgumentParser(description = "Download and prepare movie poster dataset")
+parser.add_argument("--save_dir", type=str, required=True, help="Where to save images")
+parser.add_argument("--metadata_path", type=str, required=True, help="Where to save metadata CSV")
+
+os.makedirs(args.save_dir, exist_ok=True)
+
+print("Loading dataset...")
+
+ds = load_dataset("skvarre/movie_posters-100k", streaming=True, split='train')
+
+def clean_example(example):
+    genres = example.get("genres")
+    release_date = example.get("release_date")
+
+    if not genres or not release_date:
+        return None
+
+    year = int(release_date[:4])
+
+    return {
+        "primary_genre": genres[0]["name"],
+        "year": year,
+        "decade": (year//10) * 10
+    }
+
+rows = []
+saved_idx = 0
+skipped = 0
+
+print("Starting cleaning loop...")
+
+for i, example in enumerate(ds):
+    cleaned_info = clean_example(example)
+
+    if cleaned_info is None:
+        skipped += 1
+        continue
+
+    img_path = os.path.join(args.save_dir, f'{saved_idx}.jpg')
+
+    if not os.path.exists(img_path):
+        img = example['image'].convert("RGB").resize((224, 224))
+        img.save(img_path, quality=95)
+
+    rows.append({
+        "idx": saved_idx,
+        "name": example.get("title"),
+        "primary_genre": cleaned_info["primary_genre"],
+        "release_year": cleaned_info["year"],
+        "release_decade": cleaned_info["decade"],
+        "img_path": img_path
+    })
+
+    saved_idx += 11
+
+    if saved_idx % 1000 == 0:
+        pd.DataFrame(rows).to_csv(args.metadata_path, index=False)
+        print(f'Saved {saved_idx} examples')
+
+df = pd.DataFrame(rows)
+df.to_csv(args.metadata_path, index=False)
